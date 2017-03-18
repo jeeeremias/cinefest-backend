b3110fc1473293c40086c9cd129d0666da985af5
Jeremias Pereira <jeremias.livre@gmail.com>
2017-03-18 16:58:46 -0300

    Implemented specification of Movie

--- src/main/java/com/cinefest/specification/MovieSpecification.java
+++ src/main/java/com/cinefest/specification/MovieSpecification.java
@@ -11,18 +11,31 @@ import javax.persistence.criteria.Root;
 
 public class MovieSpecification implements Specification<MovieEntity> {
 
-    private SearchCriteria searchCriteria;
+    private SearchCriteria criteria;
 
     @Override
     public Predicate toPredicate(Root<MovieEntity> root, CriteriaQuery<?> criteriaQuery, CriteriaBuilder criteriaBuilder) {
-        return null;
+        if (criteria.getValue().startsWith(">")) {
+            return criteriaBuilder.greaterThanOrEqualTo(
+                    root.get(criteria.getKey()), criteria.getValue());
+        }
+        if (criteria.getValue().startsWith("<")) {
+            return criteriaBuilder.lessThanOrEqualTo(
+                    root.get(criteria.getKey()), criteria.getValue());
+        }
+        if (criteria.getValue().startsWith("\"")) {
+            return criteriaBuilder.equal(
+                    root.get(criteria.getKey()), criteria.getValue().substring(1, criteria.getValue().length()));
+        }
+        return criteriaBuilder.like(
+                root.get(criteria.getKey()), criteria.getValue());
     }
 
     public MovieSpecification(SearchCriteria searchCriteria) {
-        this.searchCriteria = searchCriteria;
+        this.criteria = searchCriteria;
     }
 
     public SearchCriteria getSearchCriteria() {
-        return searchCriteria;
+        return criteria;
     }
 }
