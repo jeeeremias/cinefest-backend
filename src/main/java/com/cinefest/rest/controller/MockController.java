aeb084028cc9a7b79545b7800f580f32484905e4
Jeremias Pereira <jeremias.livre@gmail.com>
2017-06-04 13:04:21 -0300

    Refactoring wrong usage of inheritance

--- src/main/java/com/cinefest/rest/controller/MockController.java
+++ src/main/java/com/cinefest/rest/controller/MockController.java
@@ -7,10 +7,12 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 import java.util.List;
 
+@RestController
 public class MockController {
 
     @Autowired
--- src/main/java/com/cinefest/rest/facade/MovieRestFacade.java
+++ src/main/java/com/cinefest/rest/facade/MovieRestFacade.java
@@ -1,42 +1,43 @@
 package com.cinefest.rest.facade;
 
-import com.cinefest.entity.MovieEntity;
 import com.cinefest.pojo.dto.MovieDTO;
 import com.cinefest.pojo.params.QueryCriteria;
 import com.cinefest.pojo.params.SearchCriteria;
+import com.cinefest.rest.util.converter.PagingAndSortingParamsConverter;
 import com.cinefest.service.MovieService;
 import com.cinefest.specification.MovieSpecification;
 import com.cinefest.util.converter.MovieConverter;
 import com.cinefest.util.enumeration.MovieAttr;
 import com.cinefest.util.enumeration.QueryOperator;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
-import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
 @Service
-public class MovieRestFacade extends BaseRestFacade<MovieDTO> {
+public class MovieRestFacade {
 
 	@Autowired
-    MovieService movieService;
+    private MovieService movieService;
 
-    @Override
+    @Autowired
+    private PagingAndSortingParamsConverter pagingAndSortingParamsConverter;
+
 	public List<MovieDTO> getAll(Map<String, String> params) {
-	    SearchCriteria searchCriteria = toQueryParams(params);
+        SearchCriteria searchCriteria = null;
+        if (params != null) {
+            searchCriteria = toQueryParams(params);
+        }
 		return MovieConverter.entitiesToDtos(movieService.getAll(searchCriteria));
 	}
 
-    @Override
     public MovieDTO getOne(Long id) {
         return MovieConverter.entityToDto(movieService.getOne(id));
     }
 
-    @Override
-    protected SearchCriteria toQueryParams(Map<String, String> params) {
+    private SearchCriteria toQueryParams(Map<String, String> params) {
         SearchCriteria searchCriteria = new SearchCriteria();
         searchCriteria.setPagingAndSortingParams(pagingAndSortingParamsConverter.convertToQueryParam(params));
 
