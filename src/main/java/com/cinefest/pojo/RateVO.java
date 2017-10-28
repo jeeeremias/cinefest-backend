ec5bb246407824b00c250fb4a8bb385e2038ad6f
Jeremias Reis <jeremias.pereiraa@gmail.com>
2017-10-28 00:49:00 -0200

    New VOS

--- src/main/java/com/cinefest/pojo/RateVO.java
+++ src/main/java/com/cinefest/pojo/RateVO.java
@@ -1,5 +1,43 @@
 package com.cinefest.pojo;
 
+import java.time.LocalDateTime;
+
 public class RateVO {
 
+  private long id;
+  private double rate;
+  private LocalDateTime dateTime;
+  private UserVO user;
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+  public double getRate() {
+    return rate;
+  }
+
+  public void setRate(double rate) {
+    this.rate = rate;
+  }
+
+  public LocalDateTime getDateTime() {
+    return dateTime;
+  }
+
+  public void setDateTime(LocalDateTime dateTime) {
+    this.dateTime = dateTime;
+  }
+
+  public UserVO getUser() {
+    return user;
+  }
+
+  public void setUser(UserVO user) {
+    this.user = user;
+  }
 }
--- src/main/java/com/cinefest/pojo/UserVO.java
+++ src/main/java/com/cinefest/pojo/UserVO.java
@@ -1,32 +1,17 @@
-package com.cinefest.entity;
+package com.cinefest.pojo;
 
 import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
 import java.time.LocalDate;
 
-@Entity
-public class UserEntity {
+public class UserVO {
 
-  @Id
-  @GeneratedValue
   private String id;
-
-  @Column
   private String firstName;
-
-  @Column
   private String lastName;
-
-  @Column
   private String email;
-
-  @Column
   private String cellPhone;
-
-  @Column
   private LocalDate bornDate;
+  private boolean privateEmail;
 
   public String getId() {
     return id;
@@ -74,5 +59,13 @@ public class UserEntity {
 
   public void setBornDate(LocalDate bornDate) {
     this.bornDate = bornDate;
+  }
+
+  public boolean isPrivateEmail() {
+    return privateEmail;
+  }
+
+  public void setPrivateEmail(boolean privateEmail) {
+    this.privateEmail = privateEmail;
   }
 }
