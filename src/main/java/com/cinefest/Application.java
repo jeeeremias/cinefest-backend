4b9fc2c8677108724dfd83749f10d166075d229e
Jeremias Reis <jeremias.pereiraa@gmail.com>
2017-10-28 17:02:16 -0200

    Fixed issue with database

--- src/main/java/com/cinefest/Application.java
+++ src/main/java/com/cinefest/Application.java
@@ -2,15 +2,16 @@ package com.cinefest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
 
-@Configuration
-@EnableAutoConfiguration
 @EnableJpaRepositories
-@ComponentScan
+@SpringBootApplication
 public class Application {
 
   public static void main(String[] args) {
